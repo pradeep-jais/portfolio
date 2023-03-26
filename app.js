cc44370fd853270c91f00ba38dd444054cf15752
pradeep-jais <pradeepjais13@gmail.com>
2023-03-26 16:07:56 +0530

    icon name hint on hover fixed

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// JS code goes here
+// Main JavaScript
+
+import get from './utils/getElement.js';
+
+const toggleBtn = get('.toggle-btn');
+const closeBtn = get('.close-btn');
+const sidebar = get('.sidebar');
+
+// Toggle sidebar menu on small screen
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('show');
+});
+
+// Display icon name hint on hover
+const hintText = get('.hint-text');
+const socialLinks = [...document.querySelectorAll('.social-icons')];
+const hero = get('.hero');
+
+socialLinks.forEach((socialLink) => {
+  socialLink.addEventListener('mouseover', (e) => {
+    if (e.target.parentElement.classList.contains('social-icon')) {
+      const iconName = e.target.parentElement.dataset.label;
+      const tempRect =
+        e.target.parentElement.parentElement.getBoundingClientRect();
+      const top = tempRect.bottom;
+      const center = (tempRect.left + tempRect.right) / 2;
+      hintText.style.left = `${center}px`;
+      hintText.style.top = `${top}px`;
+      hintText.classList.add('show');
+      hintText.textContent = iconName;
+      console.log(hintText);
+    }
+  });
+});
+
+sidebar.addEventListener('mouseover', (e) => {
+  if (!e.target.parentElement.classList.contains('social-icon')) {
+    hintText.classList.remove('show');
+  }
+});
+hero.addEventListener('mouseover', (e) => {
+  if (!e.target.parentElement.classList.contains('social-icon')) {
+    hintText.classList.remove('show');
+  }
+});
