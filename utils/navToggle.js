a1f7094d93a621f479f343c80cb344320cf15423
pradeep-jais <pradeepjais13@gmail.com>
2023-04-12 21:11:27 +0530

    navabr made fixed on scroll

--- utils/navToggle.js
+++ utils/navToggle.js
@@ -1,5 +1,19 @@
 import get from './getElement.js';
 
+// fixed navbar on scroll
+const navbar = get('.navbar');
+
+window.addEventListener('scroll', () => {
+  const navHeight = navbar.getBoundingClientRect().height;
+  const scrollHeight = window.scrollY;
+  // console.log(scrollHeight);
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fix-navbar');
+  } else {
+    navbar.classList.remove('fix-navbar');
+  }
+});
+
 const navToggle = () => {
   const toggleBtn = get('.toggle-btn');
   const closeBtn = get('.close-btn');
