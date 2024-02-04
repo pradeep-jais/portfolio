c6791df52df2d6ce83a0def90d0dd64ccf4895d9
pradeep-jais <pradeepjais13@gmail.com>
2024-02-04 23:11:06 +0530

    theme setting saved to local storage to avoid losing dark theme on page refresh

--- utils/darkTheme.js
+++ utils/darkTheme.js
@@ -1,12 +1,20 @@
 const themes = [...document.querySelectorAll('.theme-color')];
 
+window.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('theme')) {
+    document.documentElement.classList.add('theme-dark');
+  }
+});
+
 themes.forEach((theme) => {
   theme.addEventListener('click', (e) => {
     if (e.target.classList.contains('light')) {
       document.documentElement.classList.remove('theme-dark');
+      localStorage.clear('theme');
     }
     if (e.target.classList.contains('dark')) {
       document.documentElement.classList.add('theme-dark');
+      localStorage.setItem('theme', 'theme-dark');
     }
   });
 });
