6a375b71f134363e3161347afd0f0ac3c97431df
pradeep-jais <pradeepjais13@gmail.com>
2024-06-22 15:04:58 +0530

    Waves, curves and animated blob used to enhance portfolio by looks and feel

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ hintTextOnIconHover();
 
 // true value indicating about the projects are displayed on home page. According to that relative image path is adjusted
 displayProjects(projects, true);
+
+// Animate hero picture blob
+const animateBlob = KUTE.fromTo(
+  '#blob1',
+  { path: '#blob1' },
+  { path: '#blob2' },
+  { repeat: 999, duration: 4000, yoyo: true }
+);
+animateBlob.start();
