f69a473292c5ce47f8f2b55397531244c3ec8009
pradeep-jais <pradeepjais13@gmail.com>
2026-05-06 14:30:56 +0530

    initial commit: code migration from vanilla js portfolio into react

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio || Pradeep Kumar
+
+It's my new portfolio app in ReactJS migrated from vanilla JavaScript for faster development experience and optimized - high performance application.
+
+(Visit legacy portfolio)[https://pradeep-jais.github.io/portfolio/]
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>My new portfolio</h1>
+      <p>
+        Doing portfolio website code migration from vanilla JavaScript into
+        React
+      </p>
+    </>
+  );
+}
+
+export default App;
