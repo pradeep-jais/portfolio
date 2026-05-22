47a50abd0a6425682dde70747cf799f11422f24e
pradeep-jais <pradeepjais13@gmail.com>
2026-05-22 17:34:17 +0530

    feat: nav fix on scroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,35 @@
+import { useRef } from "react";
 import logoImg from "../../assets/images/pjais-logo.jpg";
 import Sidebar from "../Sidebar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  const [navHeight, setNavHeight] = useState(80);
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    if (navRef.current) {
+      setNavHeight(navRef.current.getBoundingClientRect().height || 80);
+    }
+
+    const handleScroll = () => {
+      if (window.scrollY > navHeight) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const toggleTheme = () => {
     if (document.documentElement.className === "theme-light") {
@@ -21,50 +45,56 @@ const Navbar = () => {
         isOpen={isSidebarOpen}
         closeSidebar={() => setIsSidebarOpen(false)}
       />
-      <nav className="navbar">
-        <div className="nav-center section-center">
-          <NavLink to="/" className="logo">
-            <img src={logoImg} alt="logo image" className="logo-img" />
-            <h3 className="logo-text">
-              pradeep <span>jais.</span>
-            </h3>
-          </NavLink>
-          <ul className="nav-links show">
-            <li>
-              <NavLink to="/" className="nav-link">
-                home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" className="nav-link">
-                about
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/projects" className="nav-link">
-                projects
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact" className="nav-link">
-                contact
-              </NavLink>
-            </li>
-            <li>
-              <button className="theme-btn" onClick={toggleTheme}>
-                <i class="fa-solid fa-lightbulb"></i>
-              </button>
-            </li>
-          </ul>
-          <button
-            className="toggle-btn"
-            type="menu"
-            onClick={() => setIsSidebarOpen(true)}
-          >
-            <i className="fa-solid fa-bars"></i>
-          </button>
-        </div>
-      </nav>
+      <div
+        style={{
+          paddingBottom: scrolled ? `${navHeight}px` : "0px",
+        }}
+      >
+        <nav className={`navbar ${scrolled ? "fix-navbar" : ""}`} ref={navRef}>
+          <div className="nav-center section-center">
+            <NavLink to="/" className="logo">
+              <img src={logoImg} alt="logo image" className="logo-img" />
+              <h3 className="logo-text">
+                pradeep <span>jais.</span>
+              </h3>
+            </NavLink>
+            <ul className="nav-links show">
+              <li>
+                <NavLink to="/" className="nav-link">
+                  home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" className="nav-link">
+                  about
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/projects" className="nav-link">
+                  projects
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact" className="nav-link">
+                  contact
+                </NavLink>
+              </li>
+              <li>
+                <button className="theme-btn" onClick={toggleTheme}>
+                  <i className="fa-solid fa-lightbulb"></i>
+                </button>
+              </li>
+            </ul>
+            <button
+              className="toggle-btn"
+              type="menu"
+              onClick={() => setIsSidebarOpen(true)}
+            >
+              <i className="fa-solid fa-bars"></i>
+            </button>
+          </div>
+        </nav>
+      </div>
     </>
   );
 };
